a37ebb379bc70d9cba5c01dc6531e77505cb4fe8
juliusheese <julius.42.shapiro@gmail.com>
2020-09-30 19:30:59 -0500

    todolist done?

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,2 +1,23 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React from "react";
+import Todo from "./Todo";
+
+class TodoList extends React.Component {
+    render() {
+        return (
+            <div>
+                {
+                    this.props.todoList.map(i =>
+                        <Todo
+                            i={i}
+                            k={i.id}
+                            tI={this.props.tI}
+                        />)
+                }
+            </div>
+        )
+    }
+}
+
+export default TodoList;
